7a837a517f50c395358f85f347e32e114e314a02
Jákuo Viljam Dam <jayveedees@gmail.com>
2022-03-14 12:39:31 +0100

    MAUI setup

---
+++ Totalview TIme Smartclient/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Totalview_TIme_Smartclient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
