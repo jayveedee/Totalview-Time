c6839798a5ca97512ed4e62413092457fd0db0df
Jákup <jayveedees@gmail.com>
2022-03-14 13:52:16 +0100

    Jayveedee/3/setup maui project (#4) (#5)
    * MAUI setup
    
    * Added solution

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Totalview-Time
-Totalview Time is a application that is able to change and show Totalview registrations.
+
---
+++ Totalview TIme Smartclient/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Totalview_TIme_Smartclient
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
