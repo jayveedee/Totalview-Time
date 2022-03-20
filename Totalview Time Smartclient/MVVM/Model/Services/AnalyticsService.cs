dddaa3d3c0e482a2ecde0e45857877178a92e3b1
Jákuo Viljam Dam <jayveedees@gmail.com>
2022-03-20 04:49:47 +0100

    Added more fonts, analytics service & file service

---
+++ Totalview Time Smartclient/MVVM/Model/Services/AnalyticsService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System.Diagnostics;
+using Totalview_Time_Smartclient.MVVM.Model.Util;
+
+namespace Totalview_Time_Smartclient.MVVM.Model.Services
+{
+    public interface IAnalyticsService
+    {
+        void Start(bool trackDebugEvents);
+        void TrackEvent(AnalyticsServiceUtil.Event trackedEvent, AnalyticsServiceUtil.Category trackedCategory, string trackedContent);
+    }
+
+    public class AnalyticsService : IAnalyticsService
+    {
+        private readonly string _app_secret = "0f78a403-b6ea-4fe6-bc7e-d6c47c9b381c";
+
+        public AnalyticsService() { }
+
+        public void Start(bool trackDebugEvents)
+        {           
+            if (Debugger.IsAttached && !trackDebugEvents)
+            {
+                return;
+            }
+            AppCenter.Start(_app_secret, typeof(Analytics), typeof(Crashes));
+        }
+
+        public void TrackEvent(AnalyticsServiceUtil.Event trackedEvent, AnalyticsServiceUtil.Category trackedCategory, string trackedContent)
+        {
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            keyValuePairs.Add(trackedCategory.ToString(), trackedContent);
+            Analytics.TrackEvent(trackedEvent.ToString(), keyValuePairs);
+        }
+    }
+}
---
+++ Totalview Time Smartclient/MVVM/Model/Util/AnalyticsServiceUtil.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Totalview_Time_Smartclient.MVVM.Model.Util
+{
+    public static class AnalyticsServiceUtil
+    {
+        public enum Event
+        {
+            [Display(Name = "Startup")]
+            Startup,
+            [Display(Name = "Navigation")]
+            Navigation,
+            [Display(Name = "Action")]
+            Action,
+            [Display(Name = "Error")]
+            Error
+        }
+        public enum Category
+        {
+            // Startup Categories
+            [Display(Name = "Locale")]
+            Locale,
+            [Display(Name = "Server Version")]
+            ServerVersion,
+            [Display(Name = "App Version Code")]
+            AppVersionCode,
+            [Display(Name = "App Build Version")]
+            AppBuildVersion,
+
+            // Navigation Categories
+            [Display(Name = "Page Session")]
+            PageSession,
+
+            // Action Categories
+            [Display(Name = "Touch")]
+            Touch,
+            [Display(Name = "Touch Long")]
+            TouchLong,
+            [Display(Name = "Swipe")]
+            Swipe,
+
+            // Error Categories
+            [Display(Name = "Custom Error Message")]
+            CustomErrorMessage,
+            [Display(Name = "Error Message")]
+            ErrorMessage,
+            [Display(Name = "Error Type")]
+            ErrorType,
+            [Display(Name = "Exception Message")]
+            ExceptionMessage,
+            [Display(Name = "ExceptionType")]
+            ExceptionType
+        }
+    }
+}
