ada738212169174185cc44527d6a761cb9746512
Jákup <jayveedees@gmail.com>
2022-06-17 07:07:52 +0200

    Revert "merge main with develop (#14)" (#15)
    This reverts commit c8d0aa8efcc45ad6aad0fa0e5129eb14105720a1.

--- Totalview Time MAUI/Common/View/Controls/Login/TitleGrid.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Totalview_Time_MAUI;
-
-public partial class TitleGrid : Grid
-{
-	public TitleGrid()
-	{
-		InitializeComponent();
-	}
-}
--- Totalview Time MAUI/Common/ViewModel/Controls/NavigationTabs/Overview/RegistrationBarGridViewmodel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Totalview_Time_MAUI.Common.ViewModel.Controls.NavigationTabs.Overview;
-
-internal partial class RegistrationBarGridViewmodel : BaseViewModel
-{
-    
-}
